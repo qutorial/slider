f6235044fe8ca7d0941cfcf1816413df91fae89b
Zaur <qutorial@gmail.com>
2017-07-29 13:23:40 +0200

    Installer stuff

---
+++ clone.hieroglyph.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/nyergler/hieroglyph.git
---
+++ install.sh
@@ -0,0 +1,6 @@
+virtualenv --python=`which python2` ./env2
+
+. activate.sh
+
+pip install --upgrade pip
+pip install -r requirements.txt
